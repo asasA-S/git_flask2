dd342dd9ee5af10e88b2351a8879d6e5565ec89c
onishi haruya <h.oonishi.sys20@morijyobi.ac.jp>
2021-03-29 11:44:49 +0900

    menu2_2新規作成 計算結果出力

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,6 +13,14 @@ def menu():
 @app.route("/menu2")
 def menu2():
     return render_template("menu2.html")    
+
+@app.route("/menu2_2")
+def menu2_2():
+    h = request.args.get("h")
+    h = int(h)
+    en = h * 2 * 3.14
+    men = h * h * 3.14
+    return render_template("menu2_2.html",en=en,men=men)    
 
 if __name__ == "__main__":
     app.run(debug=True)
